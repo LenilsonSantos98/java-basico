1e58e12fd04c096934e54df7c63ed54cc61ff2a9
rodrigo <lenilsonrodrigo123@gmail.com>
2024-05-14 10:53:48 -0300

    Identeção-boletim-estudantil

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,29 @@
+// trabalhando a identação
+//sem identação, percebe-se que a leitura fica complicada.
+/*
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+int mediaFinal = 6;
+if(mediaFinal<6)	
+System.out.println("REPROVADO"); 
+else if(mediaFinal==6)
+System.out.println("PROVA MINERVA"); 
+else
+System.out.println("APROVADO");
+    }
+}
+*/
+
+// Agora o mesmo algoritimo só que identado:
+//Percebe-se que a leituro do algoritimo ficou mais clara.
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+    int mediaFinal = 6;
+        if(mediaFinal<6)	
+            System.out.println("REPROVADO"); 
+        else if(mediaFinal==6)
+            System.out.println("PROVA MINERVA"); 
+        else
+            System.out.println("APROVADO");
+    }
+}
