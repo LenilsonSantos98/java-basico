9553ef7de85cd45b9d794da2affc02c782f1234c
rodrigo <lenilsonrodrigo123@gmail.com>
2024-05-14 10:42:04 -0300

    minha classe

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,16 @@
+public class MinhaClasse {
+
+public static void main(String [] args) {
+   
+String primeiroNome = "Lenilson";
+String segundoNome = "Santos";
+
+String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+System.out.println(nomeCompleto);
+} 
+ 
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do metodo: " +  primeiroNome.concat(" ").concat(segundoNome);
+}
+}
